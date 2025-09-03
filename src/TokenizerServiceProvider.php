a8e1f6737f1ca62560f39626817067e053590e3d
jundayw <jundayw@126.com>
2025-09-03 15:57:25 +0800

    TokenizerServiceProvider

---
+++ src/TokenizerServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Jundayw\Tokenizer;
+
+use Illuminate\Support\ServiceProvider;
+
+class TokenizerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
