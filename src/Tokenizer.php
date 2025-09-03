842ff71f3289c66fe68a43b663504a671dc5b5e5
jundayw <jundayw@126.com>
2025-09-03 20:11:10 +0800

    Tokenizer

---
+++ src/Tokenizer.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Jundayw\Tokenizer;
+
+use Jundayw\Tokenizer\Models\AuthToken;
+
+final class Tokenizer
+{
+    /**
+     * The token model class name.
+     *
+     * @var string
+     */
+    public static string $tokenModel = AuthToken::class;
+
+    /**
+     * Set the token model class name.
+     *
+     * @param string $tokenModel
+     *
+     * @return void
+     */
+    public static function useTokenModel(string $tokenModel): void
+    {
+        self::$tokenModel = $tokenModel;
+    }
+
+    /**
+     * Get the token model class name.
+     *
+     * @return string
+     */
+    public static function tokenModel(): string
+    {
+        return self::$tokenModel;
+    }
+}
