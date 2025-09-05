abb646fffea899c75c12d8b388b46bbec7c81f59
jundayw <jundayw@126.com>
2025-09-05 19:01:45 +0800

    TokenizerServiceProvider

--- src/TokenizerServiceProvider.php
+++ src/TokenizerServiceProvider.php
@@ -2,7 +2,15 @@
 
 namespace Jundayw\Tokenizer;
 
+use Illuminate\Contracts\Auth\Factory;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
+use Jundayw\Tokenizer\Contracts\TokenModel;
+use Jundayw\Tokenizer\Grants\TokenizerGrant;
+use Jundayw\Tokenizer\Guards\TokenizerGuard;
+use Jundayw\Tokenizer\Middleware\CheckForAnyScope;
+use Jundayw\Tokenizer\Middleware\CheckScopes;
 
 class TokenizerServiceProvider extends ServiceProvider
 {
@@ -13,7 +21,22 @@ class TokenizerServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        config([
+            'auth.guards.tokenizer' => array_merge([
+                'driver'   => 'tokenizer',
+                'provider' => null,
+            ], config('auth.guards.tokenizer', [])),
+        ]);
+        if (!app()->configurationIsCached()) {
+            $this->mergeConfigFrom(__DIR__ . '/../config/tokenizer.php', 'tokenizer');
+        }
+
+        $this->app->bind(TokenModel::class, function ($app) {
+            return $app->make(Tokenizer::tokenModel());
+        });
+
+        $this->addMiddlewareAlias('scopes', CheckScopes::class);
+        $this->addMiddlewareAlias('scope', CheckForAnyScope::class);
     }
 
     /**
@@ -21,6 +44,110 @@ class TokenizerServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if ($this->app->runningInConsole()) {
+            $this->registerMigrations();
+            $this->registerPublishing();
+            $this->registerCommands();
+        }
+
+        $this->registerGuard();
+    }
+
+    /**
+     * Register the middleware.
+     *
+     * @param string $name
+     * @param string $class
+     *
+     * @return mixed
+     */
+    protected function addMiddlewareAlias(string $name, string $class): mixed
+    {
+        $router = $this->app['router'];
+
+        if (method_exists($router, 'aliasMiddleware')) {
+            return $router->aliasMiddleware($name, $class);
+        }
+
+        return $router->middleware($name, $class);
+    }
+
+    /**
+     * Register the migration file.
+     *
+     * @return void
+     */
+    protected function registerMigrations(): void
+    {
+        if (Tokenizer::shouldRunMigrations()) {
+            $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        }
+    }
+
+    /**
+     * Register the package's publishable resources.
+     *
+     * @return void
+     */
+    protected function registerPublishing(): void
+    {
+        $this->publishes([
+            __DIR__ . '/../database/migrations' => database_path('migrations'),
+        ], 'tokenizer-migrations');
+
+        $this->publishes([
+            __DIR__ . '/../config/tokenizer.php' => config_path('tokenizer.php'),
+        ], 'tokenizer-config');
+    }
+
+    /**
+     * Register the Authing Artisan commands.
+     *
+     * @return void
+     */
+    protected function registerCommands(): void
+    {
+        $this->commands([
+            // Console\KeysCommand::class,
+            // Console\SecretCommand::class,
+        ]);
+    }
+
+    /**
+     * Register the token guard.
+     *
+     * @return void
+     */
+    protected function registerGuard(): void
+    {
+        Auth::resolved(function (Factory $auth) {
+            $auth->extend('tokenizer', function ($app, string $name, array $config) use ($auth) {
+                return tap($this->makeGuard($auth, $config), function (Guard $guard) {
+                    app()->refresh('request', $guard, 'setRequest');
+                });
+            });
+        });
+    }
+
+    /**
+     * Make an instance of the token guard.
+     *
+     * @param Factory $auth
+     * @param array   $config
+     *
+     * @return TokenizerGuard
+     */
+    protected function makeGuard(Factory $auth, array $config): TokenizerGuard
+    {
+        return new TokenizerGuard(
+            new TokenizerGrant(
+                $auth,
+                $config,
+                $this->app['request'],
+                $this->app[TokenModel::class]
+            ),
+            $this->app['request'],
+            $auth->createUserProvider($config['provider'] ?? null),
+        );
     }
 }
