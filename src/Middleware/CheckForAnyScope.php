ed4e5737e8989911565b16091a6a8115086fbd10
jundayw <jundayw@126.com>
2025-09-05 19:21:27 +0800

    Middleware

---
+++ src/Middleware/CheckForAnyScope.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Jundayw\Tokenizer\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Jundayw\Tokenizer\Exceptions\AuthenticationException;
+use Jundayw\Tokenizer\Exceptions\MissingScopeException;
+
+class CheckForAnyScope
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param Request  $request
+     * @param Closure  $next
+     * @param string[] $scopes
+     *
+     * @return Response
+     * @throws AuthenticationException
+     * @throws MissingScopeException
+     */
+    public function handle(Request $request, Closure $next, string ...$scopes): Response
+    {
+        if (!$request->user() || !$request->user()->token()) {
+            throw new AuthenticationException;
+        }
+
+        foreach ($scopes as $scope) {
+            if ($request->user()->tokenCan($scope)) {
+                return $next($request);
+            }
+        }
+
+        throw new MissingScopeException($scopes);
+    }
+}
