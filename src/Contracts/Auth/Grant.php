311958687467488829c371c16d0d6c638d99c5f2
jundayw <jundayw@126.com>
2025-09-06 21:46:07 +0800

    Grant

--- src/Contracts/Auth/Grant.php
+++ src/Contracts/Auth/Grant.php
@@ -2,7 +2,36 @@
 
 namespace Jundayw\Tokenizer\Contracts\Auth;
 
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\UserProvider;
+use Illuminate\Http\Request;
+use Jundayw\Tokenizer\Contracts\Authorizable;
+
 interface Grant
 {
+    /**
+     * Retrieve the authenticated user for the incoming request.
+     *
+     * @param Request      $request
+     * @param UserProvider $provider
+     *
+     * @return Authenticatable|null
+     */
+    public function __invoke(Request $request, UserProvider $provider): ?Authenticatable;
 
+    /**
+     * Get the authorizable model used by the token.
+     *
+     * @return Authorizable
+     */
+    public function getProvider(): Authorizable;
+
+    /**
+     * Set the authorizable model used by the token.
+     *
+     * @param Authorizable $provider
+     *
+     * @return static
+     */
+    public function setProvider(Authorizable $provider): static;
 }
---
+++ src/Contracts/Auth/SupportsBasicAuth.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Jundayw\Tokenizer\Contracts\Auth;
+
+interface SupportsBasicAuth
+{
+
+}
