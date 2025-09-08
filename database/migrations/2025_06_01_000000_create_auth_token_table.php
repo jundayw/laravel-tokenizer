5b2dec0c76e8e5220d8fdac1eea01e7dce3f54dc
jundayw <jundayw@126.com>
2025-09-08 13:49:49 +0800

    Migration

--- database/migrations/2025_06_01_000000_create_auth_token_table.php
+++ database/migrations/2025_06_01_000000_create_auth_token_table.php
@@ -25,17 +25,18 @@ return new class extends Migration {
     {
         Schema::create(config('tokenizer.table', 'auth_tokens'), function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name')->index('idx_name')->comment('Token Name');
-            $table->string('package')->index('idx_package')->comment('Package-bound token types');
+            $table->string('scene')->default('default')->index('idx_scene_type')->comment('Scene Type');
             $table->morphs('tokenable', 'idx_tokenable');
+            $table->string('name')->index('idx_name')->comment('Token Name');
             $table->text('access_token')->fulltext('fti_access_token')->nullable()->comment('Access Token');
             $table->text('refresh_token')->fulltext('fti_refresh_token')->nullable()->comment('Refresh Token');
-            $table->text('scopes')->comment('Token Scopes');
-            $table->timestamp('access_token_expire_at')->index('idx_access_token_expire_at')->comment('Token expiration time');
-            $table->timestamp('refresh_token_expire_at')->index('idx_refresh_token_expire_at')->comment('Refresh token expiration time');
-            $table->timestamp('last_used_at')->nullable()->comment('Last used time');
-            $table->timestamp('created_at')->comment('Created Time');
-            $table->timestamp('updated_at')->comment('Updated Time');
+            $table->longText('scopes')->comment('Token Scopes');
+            $table->timestamp('access_token_expire_at')->index('idx_access_token_expire_at')->comment('Token Expiration Time');
+            $table->timestamp('refresh_token_available_at')->index('idx_refresh_token_available_at')->comment('Refresh Token Available Time');
+            $table->timestamp('refresh_token_expire_at')->index('idx_refresh_token_expire_at')->comment('Refresh Token Expiration Time');
+            $table->timestamp('last_used_at')->nullable()->comment('Last Used Time');
+            $table->timestamp('created_at')->nullable()->comment('Created Time');
+            $table->timestamp('updated_at')->nullable()->comment('Updated Time');
             $table->timestamp('deleted_at')->index('idx_deleted_at')->nullable()->comment('Deleted Time');
         });
     }
