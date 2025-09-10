eabf1146b3b4c436bfabc3c16e0c5876666bbfe9
jundayw <jundayw@126.com>
2025-09-10 13:42:12 +0800

    remove::SupportsBasicAuth

--- src/Contracts/Auth/SupportsBasicAuth.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Jundayw\Tokenizer\Contracts\Auth;
-
-interface SupportsBasicAuth
-{
-
-}
--- src/Contracts/Auth/SupportsTokenAuth.php
+++ src/Contracts/Auth/SupportsTokenAuth.php
@@ -6,65 +6,55 @@ use Illuminate\Contracts\Auth\Authenticatable;
 
 interface SupportsTokenAuth
 {
-    // /**
-    //  * Attempt to authenticate a user using the given credentials.
-    //  *
-    //  * @param array $credentials
-    //  * @param bool  $remember
-    //  *
-    //  * @return bool
-    //  */
-    // public function attempt(array $credentials = [], $remember = false): bool;
-    //
-    // /**
-    //  * Log a user into the application without sessions or cookies.
-    //  *
-    //  * @param array $credentials
-    //  *
-    //  * @return bool
-    //  */
-    // public function once(array $credentials = []);
-    //
-    // /**
-    //  * Log a user into the application.
-    //  *
-    //  * @param \Illuminate\Contracts\Auth\Authenticatable $user
-    //  * @param bool                                       $remember
-    //  *
-    //  * @return void
-    //  */
-    // public function login(Authenticatable $user, $remember = false);
-    //
-    // /**
-    //  * Log the given user ID into the application.
-    //  *
-    //  * @param mixed $id
-    //  * @param bool  $remember
-    //  *
-    //  * @return \Illuminate\Contracts\Auth\Authenticatable|bool
-    //  */
-    // public function loginUsingId($id, $remember = false);
-    //
-    // /**
-    //  * Log the given user ID into the application without sessions or cookies.
-    //  *
-    //  * @param mixed $id
-    //  *
-    //  * @return \Illuminate\Contracts\Auth\Authenticatable|bool
-    //  */
-    // public function onceUsingId($id);
-    //
-    // /**
-    //  * Determine if the user was authenticated via "remember me" cookie.
-    //  *
-    //  * @return bool
-    //  */
-    // public function viaRemember();
-    //
-    // /**
-    //  * Log the user out of the application.
-    //  *
-    //  * @return void
-    //  */
-    // public function logout();
+    /**
+     * Log the given user ID into the application without maintaining session state.
+     *
+     * @param mixed $id
+     *
+     * @return Authenticatable|false
+     */
+    public function onceUsingId(mixed $id): bool|Authenticatable;
+
+    /**
+     * Log a user into the application without maintaining session state.
+     *
+     * @param array $credentials
+     *
+     * @return bool
+     */
+    public function once(array $credentials = []): bool;
+
+    /**
+     * Attempt to authenticate a user using the given credentials.
+     *
+     * @param array $credentials
+     *
+     * @return bool
+     */
+    public function attempt(array $credentials = []): bool;
+
+    /**
+     * Log the given user ID into the application.
+     *
+     * @param mixed $id
+     *
+     * @return Authenticatable|false
+     */
+    public function loginUsingId(mixed $id): bool|Authenticatable;
+
+    /**
+     * Log a user into the application.
+     *
+     * @param Authenticatable $user
+     *
+     * @return void
+     */
+    public function login(Authenticatable $user): void;
+
+    /**
+     * Log the user out of the application.
+     *
+     * @return void
+     */
+    public function logout(): void;
 }
